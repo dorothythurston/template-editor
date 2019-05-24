ee531ecb4b3633a1b72a088b3ba1ecd769553bde
Dorothy Thurston <dorothy.thurston@autodesk.com>
2019-05-24 16:31:58 -0400

    Add react-testing-library

---
+++ src/components/TemplateEditor.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function TemplateEditor(props) {
+  return (
+    <div className="TemplateEditor">
+      <p>Template Editor</p>
+    </div>
+  );
+}
+
+export default TemplateEditor;
---
+++ src/components/TemplateEditor.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render, waitForElement } from 'react-testing-library';
+import TemplateEditor from './TemplateEditor';
+
+it('renders personalized greeting', async () => {
+  // Render new instance in every test to prevent leaking state
+  const { getByText } = render(<TemplateEditor />);
+
+  await waitForElement(() => getByText('Template Editor'));
+});
