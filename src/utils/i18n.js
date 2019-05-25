cf19a46b15a4f2b02d90efbdfa1fe023c9aaefcf
Dorothy Thurston <dorothy.thurston@autodesk.com>
2019-05-25 16:50:11 -0400

    Prettier more files

--- src/utils/i18n.js
+++ src/utils/i18n.js
@@ -1,14 +1,14 @@
-import en from '../locales/en.json';
+import en from "../locales/en.json";
 
 const T = {
-  translate: (string) => {
-    const keys = string.split('.');
+  translate: string => {
+    const keys = string.split(".");
     let value = en;
     keys.forEach(key => {
       value = value[key];
     });
     return value;
   }
-}
+};
 
 export default T;
